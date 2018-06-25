d1ae0ef599d0a04ee3d8d2e2448de6014731614e
Siobhan <yuriamisaki18@gmail.com>
2018-06-25 14:42:53 +0000

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,32 @@
 # dog.rb
+
+class = Dog 
+ #name =(dog.name)
+ end
+ 
+ def dog_name
+   @@name
+ #name = "Fido"
+ print "Hello, I am {#dog name} and I am a {@breed!}"
+ end 
+ 
+ #breed = (dog.name)
+ #breed = Husky
+ Fido = Husky
+end 
+end 
+
+class = Person
+  #name =(person.name)
+end 
+
+def #person_name
+@@name 
+#name = "Andrea"
+
+end 
+
+#job = artist 
+@@job
+#job = "artist"
+ print "Hello, I am {#person_name} and my job is {@job}!"
