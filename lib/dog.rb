2fa19ec945475d1b2e9b382418f6a1bdd20ab523
Siobhan <yuriamisaki18@gmail.com>
2018-06-25 14:40:34 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -13,6 +13,19 @@ class = Dog
  #breed = (dog.name)
  #breed = Husky
  Fido = Husky
+end 
+end 
+
+class = Person
+  #name =(person.name)
+end 
+
+def #person_name
+@@name 
+#name = "Andrea"
+print "Hello, I am {#person name} and my job is {@job}!"
+end 
+
  
    
  
